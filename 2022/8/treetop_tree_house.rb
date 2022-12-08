37a481b4c216503b9d06c1ce762c4377945bb710
Andrew Brown <abbrown22@gmail.com>
2022-12-08 16:53:12 -0700

    Finished problem 8, part 2

--- 2022/8/treetop_tree_house.rb
+++ 2022/8/treetop_tree_house.rb
@@ -33,9 +33,51 @@ def visible_trees_interior(grid)
     visible_trees
 end
 
+def scenic_score(trees, tree)
+    view = []
+    trees.each do |t|
+        view << t
+        break if t >= tree
+    end
+
+    view.count
+end
+
+def highest_scenic_score(grid)
+    cur_highest_scenic_score = 0
+
+    cols = grid.transpose
+    rows = grid
+
+    col_range = [*1..cols.length-2]
+    row_range = [*1..rows.length-2]
+
+    col_range.each do |col_idx|
+        row_range.each do |row_idx|
+            scenic_score = 0
+
+            tree = grid[col_idx][row_idx]
+
+            up = scenic_score(cols[row_idx].slice(0, col_idx).reverse, tree)
+            down = scenic_score(cols[row_idx].slice(col_idx+1, cols.length), tree)
+            left = scenic_score(rows[col_idx].slice(0, row_idx).reverse, tree)
+            right = scenic_score(rows[col_idx].slice(row_idx+1, rows.length), tree)
+
+            scenic_score = up * down * left * right
+
+            if scenic_score > cur_highest_scenic_score
+                cur_highest_scenic_score = scenic_score
+            end
+        end
+    end
+
+    cur_highest_scenic_score    
+end
+
 def visible_trees(grid)
     visible_trees_edge(grid) + visible_trees_interior(grid)
 end
 
 puts "(Part 1) Total trees visible outside grid: #{visible_trees(grid)}"
+puts "(Part 2) Highest scenic score: #{highest_scenic_score(grid)}"
 
